31059d68c387e64efed03471e5000d1dcaee385f
Sobit Prasad <sobitp59@gmail.com>
2023-12-11 15:48:40 +0530

    feat : search suggestions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,33 @@
-import { CircleUserRound as AvatarIcon, Menu as HamburgerIcon, Search as SearchIcon } from 'lucide-react'
+import { CircleUserRound as AvatarIcon, Menu as HamburgerIcon, Search, Search as SearchIcon } from 'lucide-react'
 import Logo from './Logo'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { sidebarToggle } from '../features/sidebar/sidebar'
+import { useEffect, useState } from 'react'
+import { YOUTUBE_SUGGESTION_API } from '../constants'
 
 function Header() {
-    
+    const [searchQuery, setSearchQuery] = useState("")
+    const [suggestions, setSuggestions] = useState<string[]>([])
+    const [showSuggestions, setShowsuggestions] = useState<boolean>(false);
     const dispatch = useDispatch()
 
+    useEffect(() => {
+         const timer = setTimeout(() => getSuggestions(), 250)
+
+         return () => {
+            clearTimeout(timer)
+         }
+    }, [searchQuery])
+
+    async function getSuggestions(){
+        try {
+            const response = await fetch(YOUTUBE_SUGGESTION_API+searchQuery);
+            const data = await response.json();
+            setSuggestions(data[1])
+        } catch (error) {
+            console.log(error)
+        }
+    }
    
   return (
     <div className='flex justify-between align-middle py-6 px-4 border-b-2'>
@@ -14,18 +35,30 @@ function Header() {
             <HamburgerIcon  onClick={() => dispatch(sidebarToggle())} className='h-auto cursor-pointer'/>
             <Logo/>
         </section>
-
-        <section className='flex justify-center pl-6 gap-4 align-middle border-[1.5px] rounded-full w-[500px] h-[45px]'>
-            <input 
-                type="text" 
-                placeholder='Search' 
-                className='w-full h-full outline-none font-manrope'
-            />
-            <button className='px-6 border-l-[1.5px] rounded-r-full bg-gray-200'>
-                <SearchIcon className='h-auto w-full' />
-            </button>
-        </section>
-
+        
+        <div className='relative'>
+            <section className='flex  justify-center pl-6 gap-4 align-middle border-[1.5px] rounded-full w-[500px] h-[45px]'>
+                <input 
+                    type="search" 
+                    placeholder='Search' 
+                    className='w-full h-full outline-none font-manrope'
+                    onChange={(e) => setSearchQuery(e?.target?.value)}
+                    value={searchQuery}
+                    onFocus={() => setShowsuggestions(true)}
+                    onBlur={() => setShowsuggestions(false)}
+                />
+                <button className='px-6 border-l-[1.5px] rounded-r-full bg-gray-200'>
+                    <SearchIcon className='h-auto w-full' />
+                </button>
+            </section>
+            {showSuggestions && suggestions.length > 0 && <ul className='absolute z-[5] bg-white w-[430px] rounded-md p-2 top-[60px] shadow-sm border-[1.5px]'>
+                {suggestions.map((suggestion) => (
+                    <li key={suggestion} className='p-2 flex justify-start align-center rounded-md hover:bg-gray-100 cursor-pointer font-manrope text-gray-500'>
+                        <Search className='w-5 h-5 mr-3'/> {suggestion}
+                    </li>
+                ))}
+            </ul>}
+        </div>
         <section className='flex justify-center align-middle'>
             <AvatarIcon className='h-auto w-[30px] text-gray-600'/>
         </section>
